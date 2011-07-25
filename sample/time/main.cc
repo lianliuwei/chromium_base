bd35a9cb7530f2d110012d56f4f3c5571955d30b
lianliuwei <lianliuwei@qq.com>
2011-07-25 10:25:55 +0800

    extension the time sample.

--- sample/time/main.cc
+++ sample/time/main.cc
@@ -7,6 +7,9 @@ int main(int argc, char** argv)
     Time::Exploded ep;
     t.LocalExplode(&ep);
     t.UTCExplode(&ep);
- 
+    TimeDelta td = TimeDelta::FromDays(2);
+    TimeDelta td1 = TimeDelta::FromDays(1);
+    TimeDelta td2 = td - td1;
+    int day = td2.InDays();
     return 0;
 }
