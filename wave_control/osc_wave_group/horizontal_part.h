4d014bfd00f163753d4cded80e26b7dffe793494
lianliuwei <lianliuwei@gmail.com>
2014-04-06 23:54:04 +0800

    add name and icon

--- wave_control/osc_wave_group/horizontal_part.h
+++ wave_control/osc_wave_group/horizontal_part.h
@@ -2,10 +2,14 @@
 
 #include "wave_control/wave_range.h"
 
+class OscWave;
+
 class HorizontalPart {
 public:
   virtual SkColor color() = 0;
   virtual bool show() = 0;
+  virtual string16 text() = 0;
+  virtual SkBitmap icon() = 0;
 
   virtual WaveRange range() = 0;
   virtual WaveRange offset_range() = 0;
@@ -13,6 +17,8 @@ public:
 
   virtual int div() = 0;
   virtual int window_size() = 0;
+
+  virtual OscWave* osc_wave() = 0;
 
 protected:
   virtual ~HorizontalPart() {}
--- wave_control/osc_wave_group/trigger_part.h
+++ wave_control/osc_wave_group/trigger_part.h
@@ -2,16 +2,23 @@
 
 #include "wave_control/wave_range.h"
 
+class OscWave;
+
 class TriggerPart {
 public:
   virtual SkColor color() = 0;
   virtual bool show() = 0;
+  virtual string16 text() = 0;
+  virtual SkBitmap icon() = 0;
 
   virtual WaveRange offset_range() = 0;
   virtual double offset() = 0;
   virtual void set_offset(double offset) = 0;
   // Relate to Y or no.
   virtual bool IsRelate() = 0;
+  virtual OscWave* trigger_wave() = 0;
+
+  virtual OscWave* osc_wave() = 0;
 
 protected:
   virtual ~TriggerPart() {}
