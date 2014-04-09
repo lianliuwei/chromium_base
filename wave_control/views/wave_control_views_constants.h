6c5311e000658f3b0b5f4d4534f28961a08a80b9
lianliuwei <lianliuwei@gmail.com>
2014-04-09 23:42:55 +0800

    add wave_control_views_constants.h

---
+++ wave_control/views/wave_control_views_constants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "base/basictypes.h"
+
+const int kBorderThickness = 1;
+const SkColor kBorderColor = SkColorSetRGB(107, 107, 107);
+const SkColor kBackgroundColor = SkColorSetRGB(10, 10, 10);
+const SkColor kAxisLineColor = SkColorSetRGB(170, 170, 170);
+const int kLineWidth = 5;
+const SkColor kAxisGridColor = SkColorSetRGB(51, 51, 51);
+const int kVGridDiv = 5;
+const int kHGridDiv = 5;
+const int kDefaultHDiv = 4;
+const int kDefaultVDiv = 5;
